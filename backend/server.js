ca80b5be125157c0bd49a4e7885d6a1bac3b1ce0
Troy Gatrell <troygatrell@Mac-mini.local>
2025-03-01 11:28:26 -0500

    Revert "feat: Enable local development for search tool"
    This reverts commit ee9cb3eb0d2c4c7e27dad83ce049f89d52a1c5a1.

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      "https://triplestamptools.onrender.com",
-      "http://localhost:3000",
-      "http://127.0.0.1:5500",
-    ],
+    origin: ["https://triplestamptools.onrender.com", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
